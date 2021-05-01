ba3a3cfb20f2b928728f2b37a05b339230cd6b6a
Volodymyr Prykhodko <volodymyr.pryhodko@yahoo.com>
2021-05-02 02:48:27 +0300

    first-commit

---
+++ script.js
@@ -0,0 +1,118 @@
+// Initializing variable for array that will have all the data
+
+let array = [];
+
+// Getting IDs of the elements that we will work with
+const addButton = document.getElementById("add");
+const deleteButton = document.getElementById("delete");
+const show = document.getElementById("show");
+
+// Function that allows to work with the value of the input and assign the value to localStorage
+
+const addItemToArray = function () {
+  const input = document.getElementById("input");
+
+  // Checking if input is empty
+  if (input.value.length === 0) {
+    alert("Input cannot be empty. Please insert at least 1 character.");
+    return;
+  }
+  // Checking if array is no longer than 26 elements as per assignment
+  else if (array.length === 26) {
+    alert("Only 26 items are allowed.");
+    return;
+  }
+  // Pushing value of input in the array and assigning it to the localStorage
+  else {
+    array.push(input.value);
+    setValueOfStorage();
+  }
+
+  // Creating new element
+  createNewElement();
+
+  // Resetting the value of the input
+  input.value = "";
+};
+
+// Function that creates new element
+const createNewElement = function () {
+  // Creating element and putting it in the variable
+  const newElement = document.createElement("div");
+
+  // Appending it to the existing element in HTML document
+  show.appendChild(newElement);
+
+  // Adding class to our created element
+  show.lastChild.setAttribute("class", "show__inner");
+
+  // Iterating through values in array and putting theirs content in elements
+  array.forEach((item) => {
+    show.lastChild.innerHTML = item;
+  });
+};
+
+// Function for deleting element
+const deleteAnElement = function () {
+  show.lastChild.remove();
+};
+
+// Function that assigns values of the array to the localStorage
+const setValueOfStorage = function () {
+  localStorage.setItem("data", JSON.stringify(array));
+};
+
+// Getting data from local storage
+const getDataFromLocalStorage = function () {
+  return JSON.parse(localStorage.getItem("data"));
+};
+
+// Rendering data from local storage
+const renderDataFromLocalStorage = function () {
+  // Assigning existing values from the local storage to array values
+  array = getDataFromLocalStorage();
+
+  // Iterating through values of the local storage
+  getDataFromLocalStorage().forEach((item) => {
+    // Creating new element
+    const newElement = document.createElement("div");
+
+    // Assigning content that equals to the value from the localStorage
+    show.appendChild(newElement).innerHTML = item;
+
+    // If element created, assign class to this element
+    if (show.lastChild) {
+      show.lastChild.setAttribute("class", "show__inner");
+    }
+  });
+};
+
+// Deleting element from array
+const deleteElementFromArray = function () {
+  // Deleting created element from HTML and deleting last element of array
+  if (show.lastChild) {
+    array.pop();
+    deleteAnElement();
+    setValueOfStorage();
+    // If array is empty alert a message
+    if (array.length === 0) {
+      alert("Array is empty!");
+    }
+  }
+};
+
+// Check if there is any data stored in localStorage
+const checkLocalStorage = function () {
+  if (JSON.parse(localStorage.getItem("data"))) {
+    return;
+  } else {
+    setValueOfStorage();
+  }
+};
+
+checkLocalStorage();
+renderDataFromLocalStorage();
+
+// Assigning event listeners to the butttons that we created in the beginning
+addButton.addEventListener("click", addItemToArray);
+deleteButton.addEventListener("click", deleteElementFromArray);
